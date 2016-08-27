5360b66b7c1f9dd59e89d7cb397661b041bab5b0
popnikos <popnikos@gmail.com>
2016-08-27 22:27:48 +0200

    Update TraitRegularExpression.php
    phpdoc

--- TraitRegularExpression.php
+++ TraitRegularExpression.php
@@ -4,8 +4,15 @@ namespace Popnikos\ScalarValidator;
 
 trait TraitRegularExpression
 {
+  /**
+   * @var string
+   */ 
   private $pattern;
   
+  /**
+   * Assume that $pattern is a valid PCRE Regular expression
+   * @param string $pattern
+   */
   function setPattern($pattern)
   {
     $this->pattern = $pattern;
