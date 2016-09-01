ad5b1b639f4de41c732eab7b5b91036c793c1973
popnikos <popnikos@gmail.com>
2016-09-01 21:46:12 +0200

    Update RangeValidator.php
    Allow any numerical expression for both lower and upper limits

--- RangeValidator.php
+++ RangeValidator.php
@@ -8,21 +8,28 @@ class RangeValidator implements ScalarValidatorAbstract
   
   /**
    * Parse the range string to retrieve informations to evaluate validation
-   * @todo Add signed number management
+   * @return array
    */
   private function parseRange()
   {
     $info = ['error'=>null];
-    if( preg_match('/^(\[|\]) # Must begin with "[" (inclusive) or "]" (exclusive)
-    (\d+)(\.\d+)? # Min to compares (a number with or without decimal)
-    ; # Predefined separator semi-column
-    (\d+)(\.\d+)? # Max to compare with (a number with or without decimal)
+    if( preg_match('/^
+    (\[|\]) # Must begin with "[" (inclusive) or "]" (exclusive)
+    ([^;]+) # Min to compares (a numerical expression)
+    ;       # Predefined separator semi-column
+    (.+)    # Max to compare with (a number with or without decimal)
     (\[|\]) # Must end with "[" (exclusive) or "]" (inclusive)
     $/x', $this->getRange(),$rangeInfos) {
-      $info['min'] =floatval($rangeInfos[2] . $rangeInfos[3]);
-      $info['max'] =floatval($rangeInfos[4] . $rangeInfos[5]);
-      $info['isMinInclusive'] = $rangeInfos[1] === '[';
-      $info['isMaxInclusive'] = $rangeInfos[6] === ']';
+      if (is_numeric($rangeInfos[2]) && is_numeric($rangeInfos[3])) {
+        $info['min'] =floatval($rangeInfos[2]);
+        $info['max'] =floatval($rangeInfos[3]);
+        $info['isMinInclusive'] = $rangeInfos[1] === '[';
+        $info['isMaxInclusive'] = $rangeInfos[4] === ']';  
+      } else {
+        $info['error'] = sprintf('$1%s is not a valid expression.'.
+          ' Both lower and upper limit must satisfy "is_numeric" condition '.
+          '(see php manual).', $rangeInfos[0]);
+      }
     } else {
       $info['error'] = 'invalid range expression';
     }
@@ -31,6 +38,7 @@ class RangeValidator implements ScalarValidatorAbstract
     
   /**
    * @param string $range Defines the range against value is compare with a simple string
+   * @return RangeValidator
    */
   public function setRange($range)
   {
@@ -38,11 +46,19 @@ class RangeValidator implements ScalarValidatorAbstract
     return $this;
   }
   
+  /**
+   * @return string The range expression
+   */ 
   protected function getRange()
   {
     return $this->range;
   }
   
+  /**
+   * Check if a given numeric value satisfy range limits
+   * Throw a E_USER_WARNING if the range expression can not be evaluated
+   * @return boolean TRUE if $value is between lower and upper limit
+   */
   public function isValid($value)
   {
     $info = $this->parseRange();
