bef4b01950ce5a3c34133dfec379be7e85bf045a
popnikos <popnikos@gmail.com>
2016-09-01 23:09:32 +0200

    Create StudlyCapsNormalizer.php

---
+++ StringNormalizer/StudlyCapsNormalizer.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Popnikos\ScalarValidator\StringNormalizer;
+
+class StudlyCapsNormalizer
+{
+    private function purify($string)
+    {
+        return preg_replace('[^0-9A-Za-z]+', ' ',$string);
+    }
+
+    private function split($string) 
+    {
+        return explode(' ', $string);
+    }
+    
+    public static function normalizeWord($string)
+    {
+        return ucfirst(strtolower($string));
+    }
+    
+    /**
+     * @param string $value Any string
+     * @return string A string in StudlyCaps
+     */
+    final public static function normalize($value)
+    {
+        $normalizer = new self();
+        return implode('',array_map(array($normalizer,'normalizeWord'),$normalizer->split($normalizer->purify($value)));
+    } 
+}
